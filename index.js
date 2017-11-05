da677e39cb9bdffd0140c5462c899ad4052ed6cd
jencekade <gaephuoc@gmail.com>
2017-11-05 22:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = "";
+
+let welcomeMessage = "Welcome to Flatbook "+currentUser +"!";
+
+let excitedMessage = "WELCOME TO FLATBOOK "+currentUser + "!";
+
+let shortGreeting = "Welcome "+currentUser;
+
